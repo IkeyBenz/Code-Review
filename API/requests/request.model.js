4b9b63e9d123ed088b424199228fccbcf3171825
Ikey Benzaken <ikey.benz@gmail.com>
2019-03-19 16:43:02 -0700

    Added subject property to request model

--- API/requests/request.model.js
+++ API/requests/request.model.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const User = require('../users/user.model');
 
 const RequestSchema = new Schema({
 
     asker: { type: Schema.Types.ObjectId, ref: 'User', required: true }, // The user that send the code-review request
     answerer: { type: Schema.Types.ObjectId, ref: 'User', required: true }, // The recipient of the request for code-review
+    subject: { type: String, required: true },                          // The subject of the code review request
     cr_request: { type: String, required: true },                       // The code chunk that came with the request
     cr_response: { type: String, default: "" },                         // The code chunk that the reviewer sends back
     date_requested: { type: Date, default: Date.now() },                // The date that the request was created
