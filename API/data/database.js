d6a12f2196472ac194ecdbf4176c2cb1b576caf5
Ikey Benzaken <ikey.benz@gmail.com>
2019-03-19 16:43:30 -0700

    [removed] unnecesary variable

--- API/data/database.js
+++ API/data/database.js
@@ -1,8 +1,6 @@
 const mongoose = require('mongoose');
 const DBURL = process.env.MONGODB_URI || 'mongodb://localhost/code-review';
 
-const Production_DB = "mongodb://heroku_wdpzsztg:prf9c9m8ucrgj119fv4d7f3cq4@ds155815.mlab.com:55815/heroku_wdpzsztg"
-
 assert = require('assert');
 
 // Allows every mongoose function to be treated as if they return a promise
