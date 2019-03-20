5f9210ded7e3b440b1c4197f773c3fd46fb48a1f
Ikey Benzaken <ikey.benz@gmail.com>
2019-03-20 13:18:35 -0700

    Added MomentJs for nice date formatting

--- API/public/scripts/main.js
+++ API/public/scripts/main.js
@@ -1,3 +1,11 @@
 $(document).ready(function () {
-    console.log("Jquery installed properly.");
+
+    /** Formats every tag with class Date to be of format '2 hours ago' */
+    $('.date').each(function () {
+        const timeCreated = $(this).text()
+            , formatted = moment(timeCreated).fromNow();
+
+        $(this).text(formatted);
+    });
+
 });
