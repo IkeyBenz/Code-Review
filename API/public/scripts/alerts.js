aeff67aaff59b01d4b363eb005fd8b8cd081273f
Ikey Benzaken <ikey.benz@gmail.com>
2019-03-24 06:02:56 -0700

    [code cleanup] more linting

--- API/public/scripts/alerts.js
+++ API/public/scripts/alerts.js
@@ -1,30 +1,28 @@
-$(document).ready(function () {
-    checkForError(window.location.href);
-    checkForSuccess(window.location.href);
-});
-
-const dismissableAlert = (text, type) => {
-    return `<div class="alert ${type} alert-dismissable fade show" role="alert">
+$(document).ready(() => {
+  const dismissableAlert = (text, type) => `<div class="alert ${type} alert-dismissable fade show" role="alert">
     ${text}
     <button type="button" class="close" data-dismiss="alert" aria-label="Close">
         <span aria-hidden="true">&times;</span>
     </button>`;
-}
 
-function checkForSuccess(link) {
+  function checkForSuccess(link) {
     if (link.indexOf('success') > -1) {
-        const msg = link.slice(link.indexOf('success') + 8);
-        const alert = dismissableAlert(decodeURIComponent(msg), 'alert-success');
+      const msg = link.slice(link.indexOf('success') + 8);
+      const alert = dismissableAlert(decodeURIComponent(msg), 'alert-success');
 
-        $('body').prepend(alert);
+      $('body').prepend(alert);
     }
-}
+  }
 
-function checkForError(link) {
+  function checkForError(link) {
     if (link.indexOf('error') > -1) {
-        const msg = link.slice(link.indexOf('error') + 6);
-        const alert = dismissableAlert(decodeURIComponent(msg), 'alert-danger');
+      const msg = link.slice(link.indexOf('error') + 6);
+      const alert = dismissableAlert(decodeURIComponent(msg), 'alert-danger');
 
-        $('body').prepend(alert);
+      $('body').prepend(alert);
     }
-}
+  }
+
+  checkForError(window.location.href);
+  checkForSuccess(window.location.href);
+});
